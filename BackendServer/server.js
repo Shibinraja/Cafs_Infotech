482cb0bfcb71fd1dee9575bbaf856783493e7284
shibinraja <shibinraja@engineer.com>
2021-02-06 02:32:38 +0530

    Static route changes

--- BackendServer/server.js
+++ BackendServer/server.js
@@ -5,11 +5,10 @@ const BodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 8080;
 
-
 app.use(cors());
 app.use(BodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(BodyParser.json({ limit: "50mb", extended: true }));
-app.use(express.static(path.join(__dirname,  "../build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 const userDetails = require("./Routes/UserDetails");
 const tableContent = require("./Routes/TableContent");
@@ -19,6 +18,10 @@ app.use(tableContent);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
+  if (!url.startsWith("/app/"))
+    // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 app.listen(PORT, () => {
